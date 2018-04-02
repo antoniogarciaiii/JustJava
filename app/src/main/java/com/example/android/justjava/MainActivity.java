fb193a72641909d5d5f9f912cc1ec5d38631d7e2
antoniogarciaiii <antoniogarciaiii@gmail.com>
2018-04-02 13:44:34 -0500

    finished lesson 6, part 21.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,27 +36,22 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         int quantity = 5;
         display(quantity);
-        displayPrice(quantity * 5);
     }
 
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        int quantity = 2;
-        quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
-        displayPrice(quantity * 5);
     }
 
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        int quantity = 2;
-        quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
-        displayPrice(quantity * 5);
     }
 
     /**
